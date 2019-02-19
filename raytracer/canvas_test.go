544df0551d8556ee2daf47833dbb767cb14df266
Tieg Zaharia <tieg.zaharia@gmail.com>
2019-02-19 15:31:12 -0500

    Adds failing tests for Canvas.WritePixel.

--- raytracer/canvas_test.go
+++ raytracer/canvas_test.go
@@ -11,3 +11,10 @@ func TestNewCanvas(t *testing.T) {
   assertEqualFloat64(t, 20, c1.Height)
 }
 
+func TestWritePixel(t *testing.T) {
+  c1 := NewCanvas(10, 20)
+  red := NewColor(1, 0, 0)
+
+  c1.WritePixel(c1, 2, 3, red)
+  assertEqualColor(red, c1.PixelAt(2, 3))
+}
