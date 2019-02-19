7db157f78286fe3466703aad544d3de15bccdd1e
Tieg Zaharia <tieg.zaharia@gmail.com>
2019-02-19 17:19:11 -0500

    Add breaking test for Canvas.ToPpm when it has data.

--- raytracer/canvas_test.go
+++ raytracer/canvas_test.go
@@ -34,3 +34,31 @@ func TestCanvasToPpm(t *testing.T) {
 
 	assertEqualString(t, expected, actual)
 }
+
+func TestCanvasToPpmWithData(t *testing.T) {
+  c1 := NewCanvas(5, 3)
+  c1.WritePixel(0, 0, Color{1.5, 0, 0})
+  c1.WritePixel(2, 1, Color{0, 0.5, 0})
+  c1.WritePixel(4, 2, Color{-0.5, 0, 1})
+  lines := strings.Split(c1.ToPpm(), "\n")
+
+  actual := strings.Join(lines[3:len(lines) - 1], "\n")
+  expected := `
+255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 128 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
+`
+  assertEqualString(t, expected, actual)
+}
+
+// Scenario: Constructing the PPM pixel data
+// Given c ← canvas(5, 3)
+// And c1 ← color(1.5, 0, 0)
+// And c2 ← color(0, 0.5, 0)
+// And c3 ← color(-0.5, 0, 1)
+// When write_pixel(c, 0, 0, c1)
+// And write_pixel(c, 2, 1, c2)
+// And write_pixel(c, 4, 2, c3)
+// And ppm ← canvas_to_ppm(c)
+// Then lines 4-6 of ppm are """
+//                      """
