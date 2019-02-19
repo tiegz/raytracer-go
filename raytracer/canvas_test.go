a751bb2fe66a4438c3a154e552dfa7a28f5c87d8
Tieg Zaharia <tieg.zaharia@gmail.com>
2019-02-19 16:30:54 -0500

    Add some more NewCanvas assertions.

--- raytracer/canvas_test.go
+++ raytracer/canvas_test.go
@@ -5,11 +5,23 @@ import (
 )
 
 func TestNewCanvas(t *testing.T) {
-	c1 := NewCanvas(10, 20)
+  c1 := NewCanvas(10, 20)
+  expectedColor := NewColor(0, 0, 0)
 
-	assertEqualInt(t, 10, c1.Width)
-	assertEqualInt(t, 20, c1.Height)
+  assertEqualInt(t, 10, c1.Width)
+  assertEqualInt(t, 20, c1.Height)
+
+  for x := 0; x < 10; x = x + 1 {
+    for y := 20; y < 20; y = y + 1 {
+      assertEqualColor(t, expectedColor, c1.PixelAt(x, y))
+    }
+  }
 }
+
+// Scenario: Creating a canvas Given c ← canvas(10, 20)
+// Then c.width = 10
+// And c.height = 20
+// And every pixel of c is color(0, 0, 0)
 
 func TestWritePixel(t *testing.T) {
 	c1 := NewCanvas(10, 20)
