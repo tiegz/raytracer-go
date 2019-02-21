bde0979244a04bf5f90ba88ffe2d4e252a82dec3
Tieg Zaharia <tieg.zaharia@gmail.com>
2019-02-21 14:59:35 -0500

    Adds scale transformation.

--- raytracer/transformation.go
+++ raytracer/transformation.go
@@ -7,3 +7,13 @@ func NewTranslation(x, y, z float64) Matrix {
 	translation.Set(2, 3, z)
 	return translation
 }
+
+func NewScale(x, y, z float64) Matrix {
+	scale := NewMatrix(4, 4, []float64{
+		x, 0, 0, 0,
+		0, y, 0, 0,
+		0, 0, z, 0,
+		0, 0, 0, 1,
+	})
+	return scale
+}
--- raytracer/transformation_test.go
+++ raytracer/transformation_test.go
@@ -15,6 +15,16 @@ func TestNewTranslation(t *testing.T) {
 	assertEqualMatrix(t, expected, actual)
 }
 
+func TestNewScale(t *testing.T) {
+	actual := NewScale(2, 3, 4)
+	expected := NewMatrix(4, 4, []float64{
+		2, 0, 0, 0,
+		0, 3, 0, 0,
+		0, 0, 4, 0,
+		0, 0, 0, 1,
+	})
+	assertEqualMatrix(t, expected, actual)
+}
 func TestMultiplyingPointByTranslationMatrix(t *testing.T) {
 	translation := NewTranslation(5, -3, 2)
 	point := NewPoint(-3, 4, 5)
@@ -35,11 +45,51 @@ func TestMultiplyingPointByInvertedTranslationMatrix(t *testing.T) {
 	assertEqualTuple(t, expected, actual)
 }
 
+// Translation has no effect on vectors.
 func TestMultiplyingVectorByTranslationMatrixHasNoEffect(t *testing.T) {
-	matrix := NewTranslation(5, -3, 2)
+	translation := NewTranslation(5, -3, 2)
 	vector := NewVector(-3, 4, 5)
 	expected := vector
-	actual := matrix.MultiplyByTuple(vector)
+	actual := translation.MultiplyByTuple(vector)
+
+	assertEqualTuple(t, expected, actual)
+}
+
+func TestScalingPointByScaleMatrix(t *testing.T) {
+	scale := NewScale(2, 3, 4)
+	point := NewPoint(-4, 6, 8)
+	expected := NewPoint(-8, 18, 32)
+	actual := scale.MultiplyByTuple(point)
+
+	assertEqualTuple(t, expected, actual)
+}
+
+// Scaling applies to both vectors and points.
+func TestScalingVectorByScaleMatrix(t *testing.T) {
+	scale := NewScale(2, 3, 4)
+	vector := NewVector(-4, 6, 8)
+	expected := NewVector(-8, 18, 32)
+	actual := scale.MultiplyByTuple(vector)
+
+	assertEqualTuple(t, expected, actual)
+}
+
+func TestScalingVectorByInvertedScaleMatrix(t *testing.T) {
+	scale := NewScale(2, 3, 4)
+	scale_inverted := scale.Inverse()
+	vector := NewVector(-4, 6, 8)
+	expected := NewVector(-2, 2, 2)
+	actual := scale_inverted.MultiplyByTuple(vector)
+
+	assertEqualTuple(t, expected, actual)
+}gst
+
+
+func TestReflectingPointByNegativeScaleMatrix(t *testing.T) {
+	scale := NewScale(-1, 1, 1) // reflect on X-axis
+	point := NewPoint(2, 3, 4)
+	expected := NewPoint(-2, 3, 4)
+	actual := scale.MultiplyByTuple(point)
 
 	assertEqualTuple(t, expected, actual)
 }
