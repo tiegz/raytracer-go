2b2b9a2a3723af98b2fdf668bb3f57c22eb573d3
Tieg Zaharia <tieg.zaharia@gmail.com>
2019-03-10 19:34:45 -0400

    Adds Camera type, with NewCamera, PixelSize, RayForPixel + Render.

---
+++ raytracer/camera.go
@@ -0,0 +1,83 @@
+package raytracer
+
+import "math"
+
+type Camera struct {
+	HSize       int
+	VSize       int
+	HalfWidth   float64
+	HalfHeight  float64
+	FieldOfView float64
+	Transform   Matrix
+}
+
+// NewCamera returns a Camera, which renders a canvas 1 unit in front of it.
+//
+// The h argument is the horizontal size of the canvas.
+// The v argument is the vertical size of the canvas.
+// The f argument is the field-of-view of the camera. (i.e. smaller means zoomed-in)
+//
+// The Camera also has a Transform attribute, describing the world's orientation relative to the camera.
+func NewCamera(h, v int, f float64) Camera {
+	return Camera{
+		HSize:       h,
+		VSize:       v,
+		FieldOfView: f,
+		Transform:   IdentityMatrix(),
+	}
+}
+
+func (c *Camera) PixelSize() float64 {
+	halfView := math.Tan(c.FieldOfView / 2) // p 106 illustration
+	aspectRatio := float64(c.HSize) / float64(c.VSize)
+
+	// TODO can we move these value settings into the constructor?
+	if aspectRatio >= 1 { // h >= v
+		c.HalfWidth = halfView
+		c.HalfHeight = halfView / aspectRatio
+	} else { // v > h
+		c.HalfWidth = halfView * aspectRatio
+		c.HalfHeight = c.HalfHeight
+	}
+
+	return (c.HalfWidth * 2) / float64(c.HSize)
+}
+
+// TODO memoize PixelSize() for this func?
+// RayForPixel returns a ray, from the camera through the point indicated.
+func (c *Camera) RayForPixel(pixelX, pixelY int) Ray {
+	// ... the offset from the edge of the canvas to the pixel's center ...
+	xOffset := (float64(pixelX) + 0.5) * c.PixelSize()
+	yOffset := (float64(pixelY) + 0.5) * c.PixelSize()
+
+	// ... the untransformed coordinates of the pixel in world-space. ...
+	// ... (remember that the camera looks toward -z, so +x is to the *left*.) ...
+	worldX := c.HalfWidth - xOffset
+	worldY := c.HalfHeight - yOffset
+
+	// ... transform the canvas point and the origin, and then compute the ray's direction vector. ...
+	// ... (remember that the canvas is at z=-1) ...
+	inverseCameraTransform := c.Transform.Inverse()
+	pixel := inverseCameraTransform.MultiplyByTuple(NewPoint(worldX, worldY, -1))
+	origin := inverseCameraTransform.MultiplyByTuple(NewPoint(0, 0, 0))
+
+	direction := pixel.Subtract(origin)
+	direction = direction.Normalized()
+
+	return NewRay(origin, direction)
+}
+
+// Render returns a canvas that renders the world from the given camera.
+func (c *Camera) Render(w World) Canvas {
+	canvas := NewCanvas(c.HSize, c.VSize)
+
+	for y := 0; y < c.VSize; y += 1 {
+		for x := 0; x < c.HSize; x += 1 {
+			r := c.RayForPixel(x, y)
+			color := w.ColorAt(r)
+			canvas.WritePixel(x, y, color)
+		}
+	}
+
+	return canvas
+}
---
+++ raytracer/camera_test.go
@@ -0,0 +1,69 @@
+package raytracer
+
+import (
+	"math"
+	"testing"
+)
+
+func TestConstructingACamera(t *testing.T) {
+	hsize := 160
+	vsize := 120
+	fieldOfView := math.Pi / 2
+	camera := NewCamera(hsize, vsize, fieldOfView)
+
+	assertEqualInt(t, 160, camera.HSize)
+	assertEqualInt(t, 120, camera.VSize)
+	assertEqualFloat64(t, math.Pi/2, camera.FieldOfView)
+	assertEqualMatrix(t, IdentityMatrix(), camera.Transform)
+}
+
+func TestPixelSizeForHorizontalCanvas(t *testing.T) {
+	c := NewCamera(200, 125, math.Pi/2)
+	assertEqualFloat64(t, 0.01, c.PixelSize())
+}
+
+func TestPixelSizeForVerticalCanvas(t *testing.T) {
+	c := NewCamera(125, 200, math.Pi/2)
+	assertEqualFloat64(t, 0.01, c.PixelSize())
+}
+
+func TestConstructingARayThroughCenterOfTheCanvas(t *testing.T) {
+	c := NewCamera(201, 101, math.Pi/2)
+	r := c.RayForPixel(100, 50)
+
+	assertEqualTuple(t, NewPoint(0, 0, 0), r.Origin)
+	assertEqualTuple(t, NewVector(0, 0, -1), r.Direction)
+}
+
+func TestConstructingARayThroughACornerOfTheCanvas(t *testing.T) {
+	c := NewCamera(201, 101, math.Pi/2)
+	r := c.RayForPixel(0, 0)
+
+	assertEqualTuple(t, NewPoint(0, 0, 0), r.Origin)
+	assertEqualTuple(t, NewVector(0.66519, 0.33259, -0.66851), r.Direction)
+}
+
+func TestConstructingARayWhenCameraIsTransformed(t *testing.T) {
+	c := NewCamera(201, 101, math.Pi/2)
+	transform := NewRotateY(math.Pi / 4)
+	transform = transform.Multiply(NewTranslation(0, -2, 5))
+	c.Transform = transform
+	r := c.RayForPixel(100, 50)
+
+	assertEqualTuple(t, NewPoint(0, 2, -5), r.Origin)
+	assertEqualTuple(t, NewVector(math.Sqrt(2)/2, 0, -(math.Sqrt(2)/2)), r.Direction)
+}
+
+func TestRenderingWorldWithCamera(t *testing.T) {
+	w := DefaultWorld()
+	c := NewCamera(11, 11, math.Pi/2)
+	from := NewPoint(0, 0, -5)
+	to := NewPoint(0, 0, 0)
+	up := NewVector(0, 1, 0)
+	c.Transform = NewViewTransform(from, to, up)
+	image := c.Render(w)
+
+	expected := NewColor(0.38066, 0.47583, 0.2855)
+	actual := image.PixelAt(5, 5)
+	assertEqualColor(t, expected, actual)
+}
