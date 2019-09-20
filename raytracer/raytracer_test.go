4d1b77cb5fa71c4b5de3245252a734dde15a8cde
Tieg Zaharia <tieg.zaharia@gmail.com>
2019-09-19 23:16:38 -0400

    DRY up test helpers.

--- raytracer/raytracer_test.go
+++ raytracer/raytracer_test.go
@@ -6,86 +6,90 @@ import (
 
 // Helpers
 
+func expectationFailure(t *testing.T, expected interface{}, actual interface{}) {
+	t.Errorf("\nExpected: %v\nActual:   %v\n", expected, actual)
+}
+
 func assert(t *testing.T, result bool) {
 	if !result {
-		t.Errorf("\nExpected %v to be true, but was false.", result)
+		expectationFailure(t, true, false)
 	}
 }
 
 func assertEqualMatrix(t *testing.T, expected, actual Matrix) {
 	if !expected.IsEqualTo(actual) {
-		t.Errorf("\nExpected\n---------\n%v\nTo equal\n-------\n%v\n", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertNotEqualMatrix(t *testing.T, expected, actual Matrix) {
 	if expected.IsEqualTo(actual) {
-		t.Errorf("\nExpected\n---------\n%v\nTo not equal\n-------\n%v\n", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualTuple(t *testing.T, expected, actual Tuple) {
 	if !expected.IsEqualTo(actual) {
-		t.Errorf("Expected %v to be equal to %v, but was not", actual, expected)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualColor(t *testing.T, expected, actual Color) {
 	if !expected.IsEqualTo(actual) {
-		t.Errorf("Expected %v to be equal to %v, but was not", actual, expected)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertNotEqualTuple(t *testing.T, expected, actual Tuple) {
 	if expected.IsEqualTo(actual) {
-		t.Errorf("Expected %v to not be equal to %v, but was equal", actual, expected)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualFloat64(t *testing.T, expected float64, actual float64) {
 	if !equalFloat64s(expected, actual) {
-		t.Errorf("Expected value to be %f, but was: %f\n", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualInt(t *testing.T, expected int, actual int) {
 	if expected != actual {
-		t.Errorf("Expected %v to be equal to %v, but was not", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualString(t *testing.T, expected string, actual string) {
 	if expected != actual {
-		t.Errorf("\nExpected:\n---------\n%v\nActual:\n-------\n%v\n", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualShape(t *testing.T, expected Shape, actual Shape) {
 	if !expected.IsEqualTo(actual) {
-		t.Errorf("\nExpected:\n---------\n%v\nActual:\n-------\n%v\n", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualIntersection(t *testing.T, expected Intersection, actual Intersection) {
 	if !expected.IsEqualTo(actual) {
-		t.Errorf("\nExpected:\n---------\n%v\nActual:\n-------\n%v\n", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualPointLight(t *testing.T, expected PointLight, actual PointLight) {
 	if !expected.IsEqualTo(actual) {
-		t.Errorf("\nExpected:\n---------\n%v\nActual:\n-------\n%v\n", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertEqualMaterial(t *testing.T, expected Material, actual Material) {
 	if !expected.IsEqualTo(actual) {
-		t.Errorf("\nExpected:\n---------\n%v\nActual:\n-------\n%v\n", expected, actual)
+		expectationFailure(t, expected, actual)
 	}
 }
 
 func assertNil(t *testing.T, object interface{}) {
 	if object != nil {
-		t.Errorf("\nExpected object to be nil, but wasn't:\n%v\n", object)
+		expectationFailure(t, nil, object)
 	}
 }
