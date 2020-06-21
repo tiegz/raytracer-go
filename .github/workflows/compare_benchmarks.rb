2e173b27fa8226569aadcde0f32726b79a6f08b7
Tieg Zaharia <tieg@tidelift.com>
2020-06-21 15:18:42 -0400

    Improvements to benchmark comparison formatting.

--- .github/workflows/compare_benchmarks.rb
+++ .github/workflows/compare_benchmarks.rb
@@ -6,19 +6,19 @@ current = JSON.parse(File.read("bench-#{ARGV[0]}.json"))
 parent = JSON.parse(File.read("bench-#{ARGV[1]}.json"))
 keys = (current.keys + parent.keys).compact.uniq
 
-max_key_length = keys.map(&:length).max + 2
+max_key_length = keys.map(&:length).max + 5
 
 puts "Comparing #{ARGV[0]} vs #{ARGV[1]}"
 puts [
-  "Bench".ljust(max_key_length),
-  "Ops/sec".ljust(10),
-  "Parent".ljust(10),
-  "Delta".ljust(10),
-  "Delta %".ljust(10),
+  "Name".ljust(max_key_length),
+  "ns/op".ljust(10),
+  "ns/op^".ljust(10),
+  "delta".ljust(10),
+  "delta %".rjust(10),
 ].join('')
 
 keys.each do |key|
-  delta = (parent[key].to_f - current[key].to_f)/parent[key].to_f.round(1) if parent[key]
+  delta = ((parent[key].to_f - current[key].to_f)/parent[key].to_f).round(1) if parent[key]
   delta_percentage = ((delta/parent[key].to_f) * 100).round(1) if delta
   puts [
     key.to_s.ljust(max_key_length),
