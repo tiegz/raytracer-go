8c7a9a16c5d73ac04a7e19f531629a46f9cf5f5d
Tieg Zaharia <tieg.zaharia@gmail.com>
2020-06-21 21:46:18 -0400

    Benchmark comparison: reorder cols and add red/green coloring.

--- .github/workflows/compare_benchmarks.rb
+++ .github/workflows/compare_benchmarks.rb
@@ -11,20 +11,28 @@ max_key_length = keys.map(&:length).max + 5
 puts "Comparing #{ARGV[0]} vs #{ARGV[1]}"
 puts [
   "Name".ljust(max_key_length),
-  "ns/op".ljust(10),
   "ns/op^".ljust(10),
+  "ns/op".ljust(10),
   "delta".ljust(10),
   "delta %".rjust(10),
 ].join('')
+
+def red(txt)
+  "\033[0;31m#{txt}\033[0m"
+end
+
+def green(txt)
+  "\033[0;32m#{txt}\033[0m"
+end
 
 keys.each do |key|
   delta = ((parent[key].to_f - current[key].to_f)/parent[key].to_f).round(1) if parent[key]
   delta_percentage = ((delta/parent[key].to_f) * 100).round(1) if delta
   puts [
     key.to_s.ljust(max_key_length),
-    current[key].to_s.ljust(10),
     parent[key].to_s.ljust(10),
+    current[key].to_s.ljust(10),
     delta.to_s.ljust(10),
-    "#{delta_percentage}%".rjust(10)
+    (delta_percentage < -1 ? red("#{delta_percentage}%") : (delta_percentage > 1 ? green("#{delta_percentage}%") : "#{delta_percentage}%")).rjust(10)
   ].join(" ")
 end
